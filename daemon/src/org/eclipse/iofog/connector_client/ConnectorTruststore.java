27fbf1b08f40c7121829cbc83ccbf4060f4b1edb
epankou <yauheni.pankou@fortegrp.net>
2019-03-18 17:25:29 +0300

    bug(fix) memory leak on keystore load (ENG-599)

--- daemon/src/org/eclipse/iofog/connector_client/ConnectorTruststore.java
+++ daemon/src/org/eclipse/iofog/connector_client/ConnectorTruststore.java
@@ -12,10 +12,7 @@
  */
 package org.eclipse.iofog.connector_client;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.KeyStore;
@@ -45,8 +42,8 @@ class ConnectorTruststore {
             KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
             char[] password = truststorePassword.toCharArray();
 
-            try {
-                ks.load(Files.newInputStream(Paths.get(truststoreFileName)), password);
+            try (InputStream is = Files.newInputStream(Paths.get(truststoreFileName))) {
+                ks.load(is, password);
                 Certificate oldCert = ((KeyStore.TrustedCertificateEntry) ks.getEntry(truststoreFileName, null)).getTrustedCertificate();
                 needToCreate = !cert.equals(oldCert);
             } catch (IOException | CertificateException | NoSuchAlgorithmException e) {
